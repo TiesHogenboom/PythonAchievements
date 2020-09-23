f83431a80d123045f5a5a71cda03d19f27018f61
Ties <tieshogenboom03@gmail.com>
2020-09-23 11:43:16 +0200

    Gedicht ge pushed

---
+++ Gedicht Pyt/Gedicht.py
@@ -0,0 +1,12 @@
+vers1 = "Ties Hogenboom is mijn naam en ik kom uit Roelofarendsveen,"
+vers2 = "Met de bus en de train kom ik 2 keer per week hier heen."
+vers3 = "In mijn vrije tijd train ik graag,"
+vers4 = "Wat ik later wil doen is nog een vraag."
+vers5 = "Ik werk ook 2 keer per week bij de plus,"
+vers6 = "Al mijn werk afkrijgen is dus soms best een klus."
+vers7 = "Taekwondo doe ik nu al 8 jaar,"
+vers8 = "En ik ben nog lang niet klaar."
+vers9 = "Ik wil dit jaar op school heel veel leren,"
+vers10 = "En ik ga dit jaar ook veel vershillende dingen proberen"
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10)
