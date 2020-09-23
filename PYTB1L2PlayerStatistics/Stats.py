6f489276931819d3b9c21930d4d2487bc8c6599f
Ties <tieshogenboom03@gmail.com>
2020-09-23 12:17:39 +0200

    Player stats erbij en Poem printing juiste naam gegeven

---
+++ PYTB1L2PlayerStatistics/Stats.py
@@ -0,0 +1,33 @@
+naam = "Ties"
+s = 18
+sp = 12
+ag = 16
+sl = 2
+c = 12
+l = 1
+w = "kicks"
+t = "bike"
+m = "lazy"
+
+nm1 = "name: " + naam
+print(nm1)
+nm2 = "strength: " + str(s)
+print(nm2)
+nm3 = "speed: " + str(sp)
+print(nm3)
+nm4 = "agility: " + str(ag)
+print(nm4)
+nm5 = "sleep: " + str(sl)
+print(nm5)
+nm6 = "communication: " + str(c)
+print(nm6)
+nm7 = "lives: " + str(l)
+print(nm7)
+nm8 = "weapon: " + w
+print(nm8)
+nm9 = "transportation: " + t
+print(nm9)
+nm10 = "mood: " + m
+print(nm10)
+
+
