1091b9e8bc185b57447ee85e0894358b49dd3291
Ties <tieshogenboom03@gmail.com>
2020-10-07 11:30:10 +0200

    Extra bijgevoegd, meer sommen

--- PYTB1L2MyFirstOperations/opr.py
+++ PYTB1L2MyFirstOperations/opr.py
@@ -3,22 +3,33 @@ spaarpot = 0
 
 print("Geld = 30")
 print("Ik koop voor 10 euro eten")
-ax = geld - 10.00
-print("geld - 10 =", ax )
+geld -= 10.00
+print("geld - 10 =", geld )
 print("Ik krijg van werk loon")
-bx = ax + 120.00
-print("geld + 120 =", bx )
+geld += 120.00
+print("geld + 120 =", geld )
 print("Ik wil de helft bewaren")
-cx = bx / 2
-print("geld / 2 =", cx )
+geld /= 2
+print("geld / 2 =", geld )
 print("Je verliest wat geld op staat")
-dx = cx % 12
-print("geld % 12 =", dx )
+geld %= 12
+print("geld % 12 =", geld )
 print("Je vader doet wat geld in je spaarpot")
 spaarpot += 40
-print ("spaarpot + 40", spaarpot )
-print("Je wilt weten hoeveel geld meer in je spaarpot zit dan dat je zelf hebt")
-dx /= spaarpot
-print("je hebt ",dx ,"zo veel geld als in je spaarpot")
+print ("spaarpot + 40 =", spaarpot )
+geld /= spaarpot
+print("je hebt ", geld ,"zo veel geld als in je spaarpot")
+geld += 9.75
+print ("Je verdubbeld je geld in de casino.")
+geld *= 2
+print ("geld * 2 =", geld)
+print ("Je wilt weten hoeveel geld je in totaal hebt")
+geld += spaarpot
+print ("Je hebt in totaal", geld ,"euro")
 
-
+print(12 * 12)
+print(12 / 2)
+print(12 + 12)
+print(12 - 2)
+print(12 % 2)
+print(12 * 10 / 2 + 100 - 10 % 10)
