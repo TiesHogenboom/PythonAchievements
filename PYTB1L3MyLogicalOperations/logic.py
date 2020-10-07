9bc9c36b8cfd87e6306125e058a8cc487911397b
Ties <tieshogenboom03@gmail.com>
2020-10-07 11:51:44 +0200

    Les 3 opdracht gemaakt

---
+++ PYTB1L3MyLogicalOperations/logic.py
@@ -0,0 +1,40 @@
+
+
+name = "erwin henraat"
+job = "teacher"
+moneyInAccount = 1300
+
+#Vervang de ** met de logische operatoren 'and' en/of 'or'
+
+#Zorg dat de if statement de functie buyABrandNewMotorcycle uitvoert als:
+# Mijn naam erwin henraat is en ik een baan heb.
+# Of als ik meer dan 10000 euro op mijn rekening heb staan.
+
+
+def buyABrandNewMotorcycle():
+    for index in range(10):
+        print(":)")
+
+if name == "erwin henraat" and job != None or moneyInAccount > 10000:
+    buyABrandNewMotorcycle()
+
+level = "5"
+itemInHand = "key"
+
+print("test1")
+
+def GateTrigger():
+    print("Gate Unlocked")
+    print("welcome to stage 2")
+
+print("test2")
+
+if level == "5" and itemInHand == "key":
+    GateTrigger()
+print("test3")
+
+
+
+
+#Maak nu voor jezelf ook een logische voorwaarde waarin je de operatoren 'and' en 'or' gebruikt.
+
