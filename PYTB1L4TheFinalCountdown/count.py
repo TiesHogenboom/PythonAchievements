f90734ca21a5a59758c25dde54d0bfcb240e0a0f
Ties <tieshogenboom03@gmail.com>
2020-10-21 12:56:57 +0200

    Countdown opdracht gemaakt

---
+++ PYTB1L4TheFinalCountdown/count.py
@@ -0,0 +1,17 @@
+
+import time
+Count = True
+Nummer = 1000
+Time = 0.010
+while ( Count == True ) :
+    Nummer -= 1
+    print (Nummer)
+    time.sleep(Time)
+    
+    if ( Nummer == 10) :
+        Time = 1
+   
+    elif ( Nummer == 0 ) :
+       print("https://rb.gy/enaq3a")
+       break;
+    
