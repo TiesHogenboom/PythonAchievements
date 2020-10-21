b904ccae03c084ea9de8629dec08dde677075683
Ties <tieshogenboom03@gmail.com>
2020-10-21 11:42:03 +0200

    Waldo Opdracht gemaakt

---
+++ PYTB1L4WheresWaldo/Waldo.py
@@ -0,0 +1,3 @@
+import random
+people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
+random.shuffle(people)
