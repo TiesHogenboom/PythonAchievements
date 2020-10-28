befc8a0f2b1b1b7a452a8275842a17d527ca313d
Ties <tieshogenboom03@gmail.com>
2020-10-28 12:12:33 +0100

    Code aangepast van shuffle opdracht

--- PYTB1L5ShuffleAndReturn/shuff.py
+++ PYTB1L5ShuffleAndReturn/shuff.py
@@ -2,9 +2,8 @@ import random
 
 def original(word):
     randomised = ''.join(random.sample(word, len(word)))
-    print(randomised)
     return original
     
-original(input("Voer je eerste woord in: "))
-original(input("Voer je tweede woord in: "))
-original(input("Voer je derde woord in: "))
+print(original(input("Voer je eerste woord in: ")))
+print(original(input("Voer je tweede woord in: ")))
+print(original(input("Voer je derde woord in: ")))
